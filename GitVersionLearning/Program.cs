613ec6794ab0126491626e30ad21f974b4555fd0
Nicholas Dechert <dechert.nicholas@gmail.com>
2021-03-08 18:49:17 -0800

    Add project files.

---
+++ GitVersionLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVersionLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
